167c21bf6bfaf9c7c380ed6450dcf8e47e081c6a
York <111544765+helloyork@users.noreply.github.com>
2023-03-14 21:39:53 -0700

    Update rtB.js

--- rtB.js
+++ rtB.js
@@ -1,10 +1,9 @@
-
 async function main() {
     alert('init');
     try {
         document.write('<body style="margin:0;"></body>');
         let e = document.createElement('div');
-        e.innerHTML=`<iframe src="${(new URL(location.href)).searchParams.get('tg')}"></iframe>`;
+        e.innerHTML=`<iframe src="${(new URL(location.href)).searchParams.get('tg')}" style="width:100%;height:100%"></iframe>`;
         document.body.appendChild(e);
     } catch (err) {
         alert('error:' + err)
