08c665bd3638caaa706588507db0af35a5efd0f7
York <111544765+helloyork@users.noreply.github.com>
2023-03-15 10:05:09 -0700

    Update rtB.js

--- rtB.js
+++ rtB.js
@@ -1,7 +1,6 @@
 async function main() {
     try {
-        document.write('<body style="margin:0;"></body>');
-        document.body.style.overflow = 'hidden';
+        document.write('<body style="margin:0;overflow:hidden;"></body>');
         let e = document.createElement('div');
         e.innerHTML=`<embed id="_ifr" src="${(new URL(location.href)).searchParams.get('tg')}" style="width:100%;height:100%"></embed>`;
         document.body.appendChild(e);
