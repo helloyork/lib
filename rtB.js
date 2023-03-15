310e3540195e6723baa7fd4fc9263e68daec56c8
York <111544765+helloyork@users.noreply.github.com>
2023-03-14 22:15:23 -0700

    Update rtB.js

--- rtB.js
+++ rtB.js
@@ -1,14 +1,9 @@
-function encodeXor(str) {
-    if (!str)
-        return str;
-    return encodeURIComponent(str.toString().split('').map((char,ind)=>ind % 2 ? String.fromCharCode(char.charCodeAt() ^ 2) : char).join(''));
-}
 async function main() {
     try {
         document.write('<body style="margin:0;"></body>');
         document.body.style.overflow = 'hidden';
         let e = document.createElement('div');
-        e.innerHTML=`<iframe id="_ifr" src="${'https://main.education/service/'+encodeXor((new URL(location.href)).searchParams.get('tg'))}" style="width:100%;height:100%"></iframe>`;
+        e.innerHTML=`<iframe id="_ifr" src="${(new URL(location.href)).searchParams.get('tg')}" style="width:100%;height:100%"></iframe>`;
         document.body.appendChild(e);
     } catch (err) {
         alert('error:' + err)
